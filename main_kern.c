2fb4e1ec44ddac13eaa09247eab6d7999296ef64
lisongqian <mail@lisongqian.cn>
2022-06-27 17:27:30 +0800

    fix error
    bpf in kern is not allowed to create map

--- main_kern.c
+++ main_kern.c
@@ -16,6 +16,12 @@
 
 extern int bpf_create_map(enum bpf_map_type map_type, int key_size,
                               int value_size, int max_entries, __u32 map_flags);
+
+struct concurrent_element {
+    struct bpf_spin_lock semaphore;
+    int count;
+};
+
 struct bpf_map_def SEC("maps") my_map = {
         .type = BPF_MAP_TYPE_HASH,
         .key_size = sizeof(int),
@@ -24,7 +30,20 @@ struct bpf_map_def SEC("maps") my_map = {
         .map_flags = BPF_F_NO_PREALLOC,
 };
 
-static __always_inline  void bpf_map_kern_example() {
+//BPF_ANNOTATE_KV_PAIR(my_map,int, struct struct concurrent_element);
+
+//int bpf_program(struct pt_regs *ctx) {
+//    int key = 0;
+//    struct concurrent_element init_value = {};
+//    struct concurrent_element *read_value;
+//    bpf_map_create_elem(&my_map, &key, &init_value, BPF_NOEXIST);
+//    read_value = bpf_map_lookup_elem(&my_map, &key);
+//    bpf_spin_lock(&read_value->semaphore);
+//    read_value->count += 100;
+//    bpf_spin_unlock(&read_value->semaphore);
+//}
+
+static __always_inline void bpf_map_kern_example() {
 
     int key, value, result;
     key = 1, value = 1234;
